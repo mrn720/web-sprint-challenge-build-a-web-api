a4d70f822a66d37645f108e87874162eb0b39ce2
mrn720 <matt.ryan.norris@gmail.com>
2021-12-12 19:26:52 -0600

    Cleaning code

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
             res.status(200).json(project)
         })
         .catch(err => {
-            res.status(500).json({message: err})
+            res.status(500).json({message: err.message})
         })
 })
 
